16d32bcd85d667e135c0264e34fa82904d5523eb
Damien Aicheh <damienaicheh@microsoft.com>
2023-12-15 17:00:10 +0000

    feat: Update README and add Swagger.

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -14,44 +14,58 @@ using Npgsql;
 using AutoMapper;
 using Newtonsoft.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 namespace CommandAPI
 {
     public class Startup
     {
-        public IConfiguration Configuration {get;}
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             var builder = new NpgsqlConnectionStringBuilder();
-            builder.ConnectionString = 
+            builder.ConnectionString =
                 Configuration.GetConnectionString("PostgreSqlConnection");
-                builder.Username = Configuration["UserID"];
-                builder.Password = Configuration["Password"];
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
 
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
             {
                 opt.Audience = Configuration["ResourceID"];
                 opt.Authority = $"{Configuration["Instance"]}{Configuration["TenantId"]}";
             });
 
-            services.AddControllers().AddNewtonsoftJson(s => {
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
                 s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander API", Version = "v1" });
+            });
         }
-        
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommandContext context)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Commander API V1");
+            });
+
             context.Database.Migrate();
             if (env.IsDevelopment())
             {
@@ -59,7 +73,7 @@ namespace CommandAPI
             }
 
             app.UseRouting();
-            
+
             app.UseAuthentication();
             app.UseAuthorization();
 
